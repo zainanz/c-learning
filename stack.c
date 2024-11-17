45724e882a6c42a927a08b3a4a35fc99b92e12a5
Zainan <zainanzaher09@gmail.com>
2024-11-17 07:41:15 +0000

    Stack Complete

--- stack.c
+++ stack.c
@@ -19,14 +19,73 @@ void push(struct Node **head, int value)
     *head = node;
 }
 
-void pop(struct Node *head)
+struct Node *pop(struct Node **head)
 {
+    struct Node *node_to_remove = *head;
+    *head = (*head)->next;
+    free(node_to_remove);
+    return node_to_remove;
 }
 
+void traverse(struct Node **head)
+{
+    struct Node *cur = *head;
+    if (cur == NULL)
+    {
+        printf("\n\nTraverse: Empty Stack.\n");
+        return;
+    }
+    printf("\nHEAD | ");
+    while (cur->next)
+    {
+        printf(" %i ->", cur->data);
+        cur = cur->next;
+    }
+    printf(" %i | TAIL\n", cur->data);
+}
+
+int contains(struct Node **head, int value)
+{
+    int counter = 0;
+    struct Node *cur = *head;
+    while (cur)
+    {
+        if (cur->data == value)
+            return counter;
+        cur = cur->next;
+        counter++;
+    }
+    return -1;
+}
 int main()
 {
     struct Node *head = NULL;
     push(&head, 5);
-    printf("%i", head->data);
+    push(&head, 2);
+    push(&head, 3);
+    push(&head, 10);
+    int where = contains(&head, 5);
+    printf("\n\n5 is %i numbers away.", where);
+    traverse(&head);
+    pop(&head);
+
+    where = contains(&head, 5);
+    printf("\n\n5 is %i numbers away.", where);
+    traverse(&head);
+
+    pop(&head);
+
+    where = contains(&head, 5);
+    printf("\n\n5 is %i numbers away.", where);
+    traverse(&head);
+
+    pop(&head);
+
+    where = contains(&head, 5);
+    printf("\n\n5 is %i numbers away.", where);
+    traverse(&head);
+
+    pop(&head);
+    traverse(&head);
     return 0;
 }
