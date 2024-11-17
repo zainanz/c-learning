8eb124fd689fd27e6a10994208d8514c5db6dd12
Zainan <zainanzaher09@gmail.com>
2024-11-17 06:26:11 +0000

    Stack init - push func

--- stack.c
+++ stack.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "node.h"
+
+struct Node *createNode(int value)
+{
+    struct Node *node = malloc(sizeof(struct Node));
+    node->data = value;
+    node->next = NULL;
+    return node;
+}
+
+void push(struct Node **head, int value)
+{
+    struct Node *cur = *head;
+    struct Node *node = createNode(value);
+    if (cur != NULL)
+        node->next = *head;
+    *head = node;
+}
+
+void pop(struct Node *head)
+{
+}
 
 int main()
 {
+    struct Node *head = NULL;
+    push(&head, 5);
+    printf("%i", head->data);
     return 0;
 }
