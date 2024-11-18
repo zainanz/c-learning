148edc7bedd22b0728327e7cbf3acbed0b255f62
Zainan <zainanzaher09@gmail.com>
2024-11-18 01:56:54 +0000

    Queue

--- queue.c
+++ queue.c
@@ -33,26 +33,75 @@ struct Queue
 
 // Queue Functionality
 
-void enqueue(struct Queue *queue, int value) {
+void initializeQueue(struct Queue *queue)
+{
+    queue->head = -1;
+    queue->tail = 0;
+}
 
+bool isFull(struct Queue *queue)
+{
+    return queue->head == MAX_SIZE ? true : false;
 };
 
-void dequeue() {
+bool isEmpty(struct Queue *queue)
+{
+    return queue->head < queue->tail;
 };
 
-bool isFull() {
+void enqueue(struct Queue *queue, int value)
+{
+    if (isEmpty(queue))
+    { // Meaning the queue is empty;
+
+        queue->head = 0;
+        queue->array[0] = value;
+        return;
+    }
+    // check if is full;
+    if (isFull(queue))
+    {
+        printf("\n Stack Overflow.");
+        return;
+    };
+    queue->array[queue->head + 1] = value;
+    queue->head++;
 };
 
-bool isEmpty() {
+int dequeue(struct Queue *queue)
+{
+    if (isEmpty(queue))
+        return -1;
 
+    return queue->array[(queue->tail)++];
 };
 
-int peek() {
-
+int peek(struct Queue *queue)
+{
+    if (queue == NULL)
+    {
+        printf("Invalid Queue");
+        return -1;
+    }
+    return queue->array[queue->head];
 };
 
 int main()
 {
     struct Queue *queue = malloc(sizeof(struct Queue));
+    initializeQueue(queue);
+    enqueue(queue, 5);
+    enqueue(queue, 10);
+    enqueue(queue, 15);
+    enqueue(queue, 20);
+    printf("Queue head should equal to 3 with value 20\n");
+    printf("Head Index: %i\n", queue->head);
+    printf("Head Value: %i\n", queue->array[queue->head]);
+    printf("PEEK: %i\n", peek(queue));
+    printf("DEQUEUE: EXPECTS 5 GOT %i\n", dequeue(queue));
+    printf("DEQUEUE: EXPECTS 10 GOT %i\n", dequeue(queue));
+    printf("DEQUEUE: EXPECTS 15 GOT %i\n", dequeue(queue));
+    printf("DEQUEUE: EXPECTS 20 GOT %i\n", dequeue(queue));
+    printf("DEQUEUE: EXPECTS -1 (EMPTY QUEUE) GOT %i\n", dequeue(queue));
     return 0;
 }
