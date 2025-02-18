27ffabbc9f6611bec2811ec6c49f052dc326ddd2
zainanz <zainanzaher09@gmail.com>
2025-02-18 14:28:17 +0000

    memccpy.c completed

--- 42LibFT/str/ft_memccpy.c
+++ 42LibFT/str/ft_memccpy.c
@@ -10,3 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memccpy(void *destination, const void *source, int c, size_t n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((char *)destination)[i] = ((char *)source)[i];
+		if (((char *)source)[i] == (char)c)
+			return (destination + i + 1);
+		i++;
+	}
+	return (NULL);
+}
+/*
+#include <string.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	char name[] = "zainaaa ali";
+	char name2[] = "zayann";
+	char *ptr = (char *)ft_memccpy(name, name2, 0, 12);
+	printf("%s\n", ptr);
+	printf("%s\n", name);
+}*/
