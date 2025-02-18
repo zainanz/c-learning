21a77369b65a34df127782de38336aa45736f1cc
zainanz <zainanzaher09@gmail.com>
2025-02-18 12:24:23 +0000

    ft_memchar norminette fixed

--- 42LibFT/str/ft_memmove.c
+++ 42LibFT/str/ft_memmove.c
@@ -6,25 +6,26 @@
 /*   By: zali <zali@student.42lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 10:13:55 by zali              #+#    #+#             */
-/*   Updated: 2025/02/18 12:17:56 by zali             ###   ########.fr       */
+/*   Updated: 2025/02/18 12:23:57 by zali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove (void *destination, const void *source, size_t num)
+void	*ft_memmove(void *destination, const void *source, size_t num)
 {
-	char 	cpy[num];
-	int	i;
+	char	*cpy;
+	int		i;
 
-	if (ft_strlen(destination) < num || ft_strlen((void *)source) < num)
-		return (NULL);
 	i = 0;
-	ft_strncpy(cpy, source, num);
+	cpy = ft_strdup((char *)source);
+	if (ft_strlen(destination) < num || ft_strlen(cpy) < num)
+		return (NULL);
 	while (i < num)
 	{
 		((char *)destination)[i] = ((char *)cpy)[i];
 		i++;
 	}
+	free(cpy);
 	return (destination);
 }
