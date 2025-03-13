f4b3cda6a8818fc9518ff0300da1d4fc2cb86b81
zainanz <zainanzaher09@gmail.com>
2025-03-13 11:50:40 +0000

    Removed unness files

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: zali <zali@student.42lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 15:50:34 by zali              #+#    #+#             */
-/*   Updated: 2025/03/13 11:41:33 by zali             ###   ########.fr       */
+/*   Updated: 2025/03/13 11:42:29 by zali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int	main(void)
 {
-	int size = ft_printf("%10s\n", "hello");
+	int size = ft_printf("%010s\n", "hello");
 	ft_printf("SIZE: %i\n", size);
 	size = printf("%10s\n", "hello");
 	ft_printf("SIZE: %i\n", size);
