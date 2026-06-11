7559f8bdeeb8e43f52881e3124c125755fd603fc
zali <zainanzaher09@gmail.com>
2026-06-11 14:02:48 +0100

    work

--- CPP/Module_01/ex04/main.cpp
+++ CPP/Module_01/ex04/main.cpp
@@ -12,16 +12,18 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
-std::string	filter(std::string &str, std::string fstr, std::string s2)
-{
-	std::string	replaced_string;
-
-	for (int i = 0; i < str.length(); i++)
-	{
-		if ( str[i] == fstr[0])
-		{
-			for (int j = 0; j < fstr.length(); j++)
+int is_same(char *fstr, char *s2){
+	while (*s2){
+		if (*fstr != *s2) return 0;
+		s2++;
+		fstr++;
+	}
+	return 1;
+}
+		/*	
+			for (unsigned int j = 0; j < fstr_len; j++)
 			{
 				if (str[i + j] != fstr[j])
 					continue ;
@@ -31,8 +33,30 @@ std::string	filter(std::string &str, std::string fstr, std::string s2)
 					i+= j + 1;
 				}
 			}
+		*/
+
+std::string	filter(std::string &str, std::string fstr, std::string s2)
+{
+	std::string	replaced_string;
+	unsigned int str_len = str.length();
+	//unsigned int fstr_len = fstr.length();
+
+	int idx = 0; 
+	int prev_idx = 0;
+	while (idx != -1)
+	{
+		idx = str.find(fstr, idx);
+		if (idx != -1){
+			replaced_string += str.substr(prev_idx, idx);
+			replaced_string += s2;
+			prev_idx = idx;
 		}
-		replaced_string += str[i];
+		else
+		{
+			replaced_string += str.substr(prev_idx, str_len);
+		}
+		printf("%d", idx);
+	return replaced_string;
 	}
 	return (replaced_string);
 }
@@ -40,22 +64,26 @@ std::string	filter(std::string &str, std::string fstr, std::string s2)
 int	main(int argc, char *argv[])
 {
 	std::string buffer;
-	std::string	f_data;
-
+	std::string output;
 	if (argc != 4)
 	{
 		std::cerr << "[ERROR] Invalid Arguments.\n";
 		std::cerr << "[ERROR] ./filter <filename> <str1> <str2>\n";
-		return (EXIT_FAILURE);
+		return (1);
 	}
-	std::ifstream	f(argv[1]);
-	while (getline(f, buffer))
-		f_data += buffer + "\n";
-	f.close();
-	std::string	new_str = filter(f_data, argv[2], argv[3]);
-	std::string	output_fname = std::string(argv[1]) + ".replace";
-	std::ofstream of(output_fname);
-	of << new_str;
-	of.close();
-	return (EXIT_SUCCESS);
+	std::fstream infile(argv[1], std::ios::in);
+	std::fstream outfile;
+	if (!infile.is_open()){
+		std::cerr << "[ERROR] Invalid filename" << std::endl;
+		return 1;
+	}
+	while (std::getline(infile, buffer, '\0')){
+		output += filter(buffer, argv[2], argv[3]);
+	}
+	std::cout << output << std::endl;
+	std::string ofname = std::string(argv[1]) + std::string(".replace");
+	outfile.open(ofname.c_str(), std::ios::out);
+	outfile << output;
+	infile.close();
+	return (0);
 }
