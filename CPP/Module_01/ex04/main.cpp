9c5f14a01e60d9b5b8d10095b067d32ce4defa9c
zali <zainanzaher09@gmail.com>
2026-06-12 18:30:15 +0100

    sed

--- CPP/Module_01/ex04/main.cpp
+++ CPP/Module_01/ex04/main.cpp
@@ -14,51 +14,25 @@
 #include <fstream>
 #include <string>
 
-int is_same(char *fstr, char *s2){
-	while (*s2){
-		if (*fstr != *s2) return 0;
-		s2++;
-		fstr++;
-	}
-	return 1;
-}
-		/*	
-			for (unsigned int j = 0; j < fstr_len; j++)
-			{
-				if (str[i + j] != fstr[j])
-					continue ;
-				if (j == fstr.length() - 1)
-				{
-					replaced_string += s2;
-					i+= j + 1;
-				}
-			}
-		*/
-
 std::string	filter(std::string &str, std::string fstr, std::string s2)
 {
-	std::string	replaced_string;
-	unsigned int str_len = str.length();
-	//unsigned int fstr_len = fstr.length();
+	std::string replaced_string;
+	size_t      idx = 0; 
+	size_t      prev_idx = 0;
 
-	int idx = 0; 
-	int prev_idx = 0;
-	while (idx != -1)
+	while (idx != std::string::npos)
 	{
-		idx = str.find(fstr, idx);
-		if (idx != -1){
-			replaced_string += str.substr(prev_idx, idx);
-			replaced_string += s2;
-			prev_idx = idx;
-		}
-		else
-		{
-			replaced_string += str.substr(prev_idx, str_len);
-		}
-		printf("%d", idx);
-	return replaced_string;
+		idx = str.find(fstr, prev_idx);
+        if (idx != std::string::npos){
+            replaced_string += str.substr(prev_idx, idx - prev_idx);
+            replaced_string += s2;
+            prev_idx = idx + fstr.length();
+        }
+        else{
+            replaced_string += str.substr(prev_idx, str.length());
+        }
 	}
-	return (replaced_string);
+    return (replaced_string);
 }
 
 int	main(int argc, char *argv[])
@@ -67,10 +41,14 @@ int	main(int argc, char *argv[])
 	std::string output;
 	if (argc != 4)
 	{
-		std::cerr << "[ERROR] Invalid Arguments.\n";
-		std::cerr << "[ERROR] ./filter <filename> <str1> <str2>\n";
+		std::cerr << "[ERROR] Invalid Arguments." << std::endl;
+		std::cerr << "[ERROR] ./filter <filename> <str1> <str2>" << std::endl;
 		return (1);
 	}
+    if (argv[2][0] == '\0'){
+        std::cerr << "[ERROR] no previous regular expression" << std::endl;
+        return (1);
+    }
 	std::fstream infile(argv[1], std::ios::in);
 	std::fstream outfile;
 	if (!infile.is_open()){
@@ -80,7 +58,6 @@ int	main(int argc, char *argv[])
 	while (std::getline(infile, buffer, '\0')){
 		output += filter(buffer, argv[2], argv[3]);
 	}
-	std::cout << output << std::endl;
 	std::string ofname = std::string(argv[1]) + std::string(".replace");
 	outfile.open(ofname.c_str(), std::ios::out);
 	outfile << output;
