86ef26a8d096610059f1a7b2cc2f201de55292f2
zainanz <zainanzaher09@gmail.com>
2026-09-20 21:43:21 +0100

    ex00 complete

--- CPP/Module_06/ex00/ScalarConverter.cpp
+++ CPP/Module_06/ex00/ScalarConverter.cpp
@@ -8,6 +8,7 @@ ScalarConverter::~ScalarConverter(){
 
 static std::string  convertStrToChar(const std::string& str){
     try {
+        if (str == "nan") return "Impossible";
         long int c = strtol(str.c_str(), NULL, 10);
         if (!isascii(static_cast<char>(c))){
             return "Impossible";
@@ -25,7 +26,9 @@ static std::string  convertStrToChar(const std::string& str){
 
 static std::string  convertStrToInt(const std::string& str){
     try {
+        if (str == "nan") return "Impossible";
         long int c = strtol(str.c_str(), NULL, 10);
+        if (c > INT_MAX || c < INT_MIN) return "Impossible";
         std::ostringstream stri;
         stri << (static_cast<int>(c));
         return stri.str();
@@ -35,14 +38,49 @@ static std::string  convertStrToInt(const std::string& str){
     }
 }
 
+static bool IsNegativeInf(const std::string& str){
+    return (str == "-inff" || str == "-inf");
+}
+
+static bool IsPositiveInf(const std::string& str){
+    return (str == "+inff" || str == "+inf");
+}
+
 static std::string  convertStrToFloat(const std::string& str){
-    (void) str;
-    return "Impossible";
+    try {
+        if (str == "nan") return "nanf";
+        if (IsNegativeInf(str)) return "-inff";
+        if (IsPositiveInf(str)) return "+inff";
+        float c = strtof(str.c_str(), NULL);
+        std::ostringstream stri;
+        if (std::fmod(c, 1.0) == 0.0)
+            stri << std::fixed << std::setprecision(1) << c;
+        else 
+            stri << std::setprecision(FLT_DIG) << c;
+        return stri.str();
+    }
+    catch (...){
+        return "Impossible";
+    }
 }
 
 static std::string  convertStrToDouble(const std::string& str){
-    (void) str;
-    return "Impossible";
+    try {
+        double c;
+        if (str == "nan") return "nan";
+        if (IsNegativeInf(str)) return "-inf";
+        if (IsPositiveInf(str)) return "+inf";
+        c = strtod(str.c_str(), NULL);
+        std::ostringstream stri;
+        if (std::fmod(c, 1.0) == 0.0)
+            stri << std::fixed << std::setprecision(1) << c;
+        else 
+            stri << std::setprecision(DBL_DIG) << c;
+        return stri.str();
+    }
+    catch (...){
+        return "Impossible";
+    }
 }
 
 void    ScalarConverter::convert(const std::string& str){
--- CPP/Module_06/ex00/ScalarConverter.hpp
+++ CPP/Module_06/ex00/ScalarConverter.hpp
@@ -3,6 +3,11 @@
 #include <climits>
 #include <cstdlib>
 #include <sstream>
+#include <limits>
+#include <iomanip>
+#include <cmath>
+#define FLT_DIG 6
+#define DBL_DIG 15
 
 class ScalarConverter {
     public:
